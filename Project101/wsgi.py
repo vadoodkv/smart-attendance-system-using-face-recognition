91c883fa8f7aa36edfb775a570b71fb900e01fd7
vadoodkv <vadoodkv@gmail.com>
2025-10-06 11:02:56 +0530

    Update wsgi.py

--- Project101/wsgi.py
+++ Project101/wsgi.py
@@ -13,5 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project101.settings')
 
+application = get_wsgi_application()
+
 app = application
+
 
